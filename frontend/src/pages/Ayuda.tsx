5a5acc67bf7fff8af960016f9b5bd461cfdf7a60
Biriato <alejandrovg98@hotmail.com>
2025-06-14 16:35:49 +0200

    f

--- frontend/src/pages/Ayuda.tsx
+++ frontend/src/pages/Ayuda.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AyudaUsuario: React.FC = () => {
   return (
-    <div className="max-w-5xl mx-auto py-10 px-4 text-gray-800">
+    <div className="max-w-5xl mx-auto py-10 px-4 text-gray-800 bg-white min-h-[calc(100vh-4rem)]">
       <h1 className="text-3xl font-bold mb-6 text-center">Centro de Ayuda - Trackd</h1>
 
       {/* Sección 1: Registro */}
